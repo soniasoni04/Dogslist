6db2ed120a89c1a271274ff6a199315c6c4ca8a5
soniapathak <sonia.soni04@gmail.com>
2019-10-02 16:24:33 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DogsListContainer from './components/DogsListContainer'
+import { Route } from 'react-router-dom'
+import DogBreedImages from './components/DogBreedImages'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <main className="App-header">
+      <Route exact path="/" component={DogsListContainer} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
+     
+      </main>
+      
     </div>
   );
 }
---
+++ src/components/DogsList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+export default class DogsList extends Component {
+    renderDogBreed(breed) {
+      return <li key={breed}>
+                <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
+            </li>
+    }
+  
+    render() {
+      const { dogBreeds } = this.props
+      return (
+        <div className="dogs-list">
+          <h1>Dogs List</h1>
+  
+          { !dogBreeds && 'Loading...' }
+  
+          {
+            dogBreeds &&
+            <ol>{ dogBreeds.map(this.renderDogBreed) }</ol>
+          }
+        </div>
+      )
+    }
+  }
